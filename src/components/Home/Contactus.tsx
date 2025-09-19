f81ba86cf867b0ef9035bfeb17518d75061e124a
zakiD9 <zakariadjerboa@gmail.com>
2025-09-19 14:00:12 +0100

    update contact us

--- src/components/Home/Contactus.tsx
+++ src/components/Home/Contactus.tsx
@@ -8,9 +8,6 @@ export default function Contact() {
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
             Contact Us
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.
-          </p>
         </div>
         <div className="lg:w-1/2 md:w-2/3 mx-auto">
           <div className="flex flex-wrap -m-2">
